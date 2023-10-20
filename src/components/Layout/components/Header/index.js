98c52062fa17892462b4d6af96c0132debab66b1
thanhbinh3010 <thanhbinh301000@gmail.com>
2023-10-20 21:11:57 +0700

    initial

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+
+const cx = classNames.bind(styles);
 function Header() {
-  return <h1> header</h1>;
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>Header</div>
+    </header>
+  );
 }
 
 export default Header;
